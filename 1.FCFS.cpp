fdaa6325744b54cf53ac50bea48b3d2d8fade816
vanamalabadrinath <130487911+vanamalabadrinath@users.noreply.github.com>
2024-11-11 13:20:23 +0530

    Add files via upload

---
+++ 1.FCFS.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()                        
+{
+    int n,bt[100],wt[100],tat[100],avwt=0,avtat=0,i,j;
+    printf("Enter total number of processes:");
+    scanf("%d",&n);
+    printf("\nEnter Process Burst Time\n");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+    wt[0]=0;
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+    printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+    }
+    avwt/=i;
+    avtat/=i;
+    printf("\n\nAverage Waiting Time:%d",avwt);
+    printf("\nAverage Turnaround Time:%d",avtat);
+    return 0;
+}
+
+
